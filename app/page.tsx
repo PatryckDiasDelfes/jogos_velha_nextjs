2987e4e204daef93bf7a974a771b32cd0e0fed1f
Patryck Dias Delfes <delfesne@gmail.com>
2023-11-17 15:00:36 -0300

    a

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+  <>
+    <header>
+
+    </header>
+
+    <section>
+        <table>
+          <tbody>
+
+          </tbody>
+        </table>
+    </section> 
+  </>
+  )
+}
