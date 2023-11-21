60a97e56e8d0792c1575aecffbb9874eb53d80b9
Patryck Dias Delfes <delfesne@gmail.com>
2023-11-21 14:13:33 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,52 @@
+"use client"
 import Image from 'next/image'
+import { useState } from 'react'
 
-export default function Home() {
+export default function Page() {
+
+  const [player, setPlayer] = useState("X")
+
+  const [table, setTable] = useState(["", "", "", "", "", "", "", "", ""])
+
+  function mark(index: number) {
+
+    if (table[index] !== "") return
+    table[index] = player
+
+    if(player == "X"){
+      setPlayer("O")
+    } else {
+      setPlayer("X")
+    }
+  }
+
   return (
-  <>
-    <header>
+    <>
+      <header className='flex flex-col items-center p-5 gap-3'>
+        <h1 className='text-[30px]'>Jogo da velha NEXT</h1>
+        <p>Sem DOM e com table</p>
+      </header>
 
-    </header>
-
-    <section>
+      <article className='flex flex-col items-center gap-10'>
         <table>
-          <tbody>
-
+          <tbody className='flex flex-col gap-3'>
+            <tr className='flex gap-3'>
+              <td onClick={() => mark(0)} className='flex items-center justify-center w-[100px] h-[100px] border border-white text-3xl hover:bg-zinc-800 text-white cursor-pointer'>{table[0]}</td>
+              <td onClick={() => mark(1)} className='flex items-center justify-center w-[100px] h-[100px] border border-white text-3xl hover:bg-zinc-800 text-white cursor-pointer'>{table[1]}</td>
+              <td onClick={() => mark(2)} className='flex items-center justify-center w-[100px] h-[100px] border border-white text-3xl hover:bg-zinc-800 text-white cursor-pointer'>{table[2]}</td>
+            </tr><tr className='flex gap-3'>
+              <td onClick={() => mark(3)} className='flex items-center justify-center w-[100px] h-[100px] border border-white text-3xl hover:bg-zinc-800 text-white cursor-pointer'>{table[3]}</td>
+              <td onClick={() => mark(4)} className='flex items-center justify-center w-[100px] h-[100px] border border-white text-3xl hover:bg-zinc-800 text-white cursor-pointer'>{table[4]}</td>
+              <td onClick={() => mark(5)} className='flex items-center justify-center w-[100px] h-[100px] border border-white text-3xl hover:bg-zinc-800 text-white cursor-pointer'>{table[5]}</td>
+            </tr><tr className='flex gap-3'>
+              <td onClick={() => mark(6)} className='flex items-center justify-center w-[100px] h-[100px] border border-white text-3xl hover:bg-zinc-800 text-white cursor-pointer'>{table[6]}</td>
+              <td onClick={() => mark(7)} className='flex items-center justify-center w-[100px] h-[100px] border border-white text-3xl hover:bg-zinc-800 text-white cursor-pointer'>{table[7]}</td>
+              <td onClick={() => mark(8)} className='flex items-center justify-center w-[100px] h-[100px] border border-white text-3xl hover:bg-zinc-800 text-white cursor-pointer'>{table[8]}</td>
+            </tr>
           </tbody>
         </table>
-    </section> 
-  </>
+      </article>
+      
+    </>
   )
 }
